9bf634ae4db2ddc47db6d43d64765f1eea5ab95b
Prahlad Kumar Popuri <prahladkumar2222@gmail.com>
2019-12-21 02:14:48 +0530

    Assignment Submission

---
+++ Assignment-1/Part-B/1.cpp
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[2][2] = {{53947,56211},{19385,43159}};
+    for(int i=0;i<2;i++){
+        int s=0,olds=1,t=1,oldt=0,r=a[i][0],oldr=a[i][1],q,t1;
+        while(r!=0){
+            q=oldr/r;
+            t1=r;
+            r=oldr-(q*r);
+            oldr = t1;
+            t1=s;
+            s = olds-(q*s);
+            olds = t1;
+            t1 = t;
+            t=oldt-(q*t);
+            oldt=t1;
+        }
+        if(oldt<0){
+            oldt+=a[i][1];
+        }
+        cout<<"Multiplicative inverse of "<<a[i][0]<<"^-1mod"<<a[i][1]<<" is "<<oldt<<endl;
+    }
+
+    return 0;
+}
